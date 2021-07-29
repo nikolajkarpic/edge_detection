4db2dbae8954b9f21655397f3ccc5af7edb7974d
Nikolaj <karpicnikolaj@gmail.com>
2021-07-29 12:22:39 +0200

    Added grayscale function

---
+++ spec/cpp/imageProccesing.h
@@ -0,0 +1,16 @@
+#ifndef IMAGEPROCCESING_H_INCLUDED
+#define IMAGEPROCCESING_H_INCLUDED
+#include"common.h"
+
+#define RED_VALUE 0.2989
+#define GREEN_VALUE 0.5870
+#define BLUE_VALUE 0.1140
+
+
+using namespace cv;
+using namespace std;
+
+image2D grayScale(image2D source);
+
+
+#endif IMAGEPROCCESING_H_INCLUDED
