9f4e332d1a19384140e8b20bc558abe9dffc2507
Nikolaj <karpicnikolaj@gmail.com>
2021-10-27 00:31:18 +0200

    Began VP, added skeleton for main and common and added simple make file

---
+++ spec/VP/common.hpp
@@ -0,0 +1,11 @@
+#ifndef COMMON_H_INCLUDED
+#define COMMON_H_INCLUDED
+
+#define SC_INCLUDE_FX
+#include <systemc>
+#include <iostream> 
+#include <vector>
+#include <fstream>
+
+
+#endif // COMMON_H_INCLUDED
---
+++ spec/VP/main.cpp
@@ -0,0 +1,6 @@
+#include "common.hpp"
+
+int sc_main(int argc, char* argv[]) { 
+
+    return 0;
+}
