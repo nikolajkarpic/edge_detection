d08918826d4430d7f9ec78d6d504fcfb7456ca5b
Nikolaj <karpicnikolaj@gmail.com>
2021-10-29 16:17:45 +0200

    Added common types

--- spec/VP/common.hpp
+++ spec/VP/common.hpp
@@ -7,5 +7,18 @@
 #include <vector>
 #include <fstream>
 
+#define KERNEL_SIZE 9
+#define BIT_WIDTH_CONV_OUT 32 //27 conv out // SC_float_type sum(10, 32)
+#define BIT_WIDTH_KERNEL 16 // 17 kernel // SC_float_type kernel_val(7, 16)
+
+typedef sc_dt :: sc_fix_fast SC_float_type;
+//typedef sc_dt :: sc_uint <11> SC_int_big_type;
+//typedef sc_dt :: sc_uint <4> SC_int_small_type; 
+typedef sc_dt :: sc_uint <8> SC_pixel_value_type; // 0 255
+typedef sc_dt :: sc_int <2> SC_conv_out_t; // -1 0 1
+
+typedef std::vector < SC_float_type > SCkernel1D;
+typedef std::vector < std::vector < SC_float_type> > SCkernel2D;
+
 
 #endif // COMMON_H_INCLUDED
