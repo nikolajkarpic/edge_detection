8c90df36d228d787f4173d58bf285afd7f42e3e2
Nikolaj <karpicnikolaj@gmail.com>
2021-11-30 17:40:16 +0100

    fix: command line agruments

--- VP/main.cpp
+++ VP/main.cpp
@@ -1,8 +1,14 @@
-#include"vp.hpp"
+#include "vp.hpp"
 
+int sc_main(int argc, char *argv[])
+{
 
-int sc_main(int argc, char* argv[]) { 
-
+    if (argc != 3)
+    {
+        cout << "VP takes two arguments, path with name to .txt file from which it reads img, and path with name to the file where to save output." << endl;
+        cout << "Example ./output /home/user/project/vp/inputFile.txt /home/user/project/vp/outputFile.txt " << endl;
+        return 0;
+    }
     vp uut("VirPlat");
     uut.soft.setPathIn(argv[1]);
     uut.soft.setPathOut(argv[2]);
--- VP/python_scripts/img_to_txt.py
+++ VP/python_scripts/img_to_txt.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 import sys
 
-if len(sys.argv) == 1 and sys.argv[1].lower == "help":
+if (len(sys.argv) == 1 and sys.argv[1].lower == "help"):
     print("Script takes two argument. Path to the image to convert to .txt and path where to save .txt file\nExample py .\img_to_txt.py C:\FTN\8_osmi_semestar\g3-2021\data\input1.png C:\FTN\8_osmi_semestar\g3-2021\data\VPinput.txt \n")
     exit()
 
@@ -11,6 +11,8 @@ if len(sys.argv) != 3:
         print("Script wasn't run properly. \nScript takes two argument. Path to the image to convert to .txt and path where to save .txt file\nExample py .\img_to_txt.py C:\FTN\8_osmi_semestar\g3-2021\data\input1.png C:\FTN\8_osmi_semestar\g3-2021\data\VPinput.txt \n")
         exit()
 
+pathIn = str(sys.argv[1])
+pathOut = str(sys.argv[2])
 
 im = cv2.imread(str(sys.argv[1]))
 im = cv2.copyMakeBorder(im,  5, 5, 5, 5, cv2.BORDER_REPLICATE)
@@ -19,7 +21,7 @@ im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
 cols = im.shape[0]
 rows = im.shape[1]
 
-with open("demo.txt" , "w") as text_file:
+with open(pathOut, "w") as text_file:
 
     print(str(cols) + "\n" + str(rows), file=  text_file)
     for x in im:
