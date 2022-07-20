8bbd105413d0ec97e928eb4a713a0888f498b32c
Nikolaj <karpicnikolaj@gmail.com>
2022-07-20 13:54:21 +0200

    bug fix

--- driver/app/main.cpp
+++ driver/app/main.cpp
@@ -1,93 +1,89 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <vector>
 
 #define BRAM_SIZE 160000
+typedef std::vector<int> bram;
 
-int bramImgArray[BRAM_SIZE];
-int bramResArray[BRAM_SIZE];
-int startReg = 0;
-int doneReg = 0;
+void writeBramImg(bram brmaImg);
+bram loadData(std::string path);
 
-void writeBramImg(const int bramArray[]);
-void loadData(string path, int bramImg[]);
-
-using namespace std;
 int main(int argc, char *argv[])
 {
     if (argc < 2)
     {
-        cout << "Program wasn't called properly.\n To run it path to the target image must be passed as an argument.\n Example: ./output /home/user/g3-2021/data/Lenna.png" << endl;
+        std::cout << "Program wasn't called properly.\n To run it path to the target image must be passed as an argument.\n Example: ./output /home/user/g3-2021/data/Lenna.png" << std::endl;
         return 0;
     }
-    string path = argv[1];
-
+    std::string path = argv[1];
+    bram loadedData;
     int choice;
 
     do
     {
-        cout << "Main Menu\n";
-        cout << "Please make your selection\n";
-        cout << "1 - Load data to an array\n";
-        cout << "2 - Write img to bram\n";
-        cout << "3 - Start IP\n";
-        cout << "4 - Read bram res\n";
-        cout << "5 - Zero crossing\n";
-        cout << "6 - Exit\n";
-        cout << "Selection: ";
-        cin >> choice;
+        std::cout << "Main Menu\n";
+        std::cout << "Please make your selection\n";
+        std::cout << "1 - Load data to an array\n";
+        std::cout << "2 - Write img to bram\n";
+        std::cout << "3 - Start IP\n";
+        std::cout << "4 - Read bram res\n";
+        std::cout << "5 - Zero crossing\n";
+        std::cout << "6 - Exit\n";
+        std::cout << "Selection: ";
+        std::cin >> choice;
 
         switch (choice)
         {
         case 1:
-            loadData(path, bramImgArray);
+            loadedData = loadData(path);
             break;
         case 2:
-            writeBramImg(bramImgArray);
+            writeBramImg(loadedData);
             break;
         case 5:
-            cout << "Goodbye!";
+            std::cout << "Goodbye!";
             return 0;
             break;
         default:
-            cout << "Main Menu\n";
-            cout << "Please make your selection\n";
-            cout << "1 - Load data to an array\n";
-            cout << "2 - Write img to bram\n";
-            cout << "3 - Start IP\n";
-            cout << "4 - Read bram res\n";
-            cout << "5 - Zero crossing\n";
-            cout << "6 - Exit\n";
-            cout << "Selection: ";
-            cin >> choice;
+            std::cout << "Main Menu\n";
+            std::cout << "Please make your selection\n";
+            std::cout << "1 - Load data to an array\n";
+            std::cout << "2 - Write img to bram\n";
+            std::cout << "3 - Start IP\n";
+            std::cout << "4 - Read bram res\n";
+            std::cout << "5 - Zero crossing\n";
+            std::cout << "6 - Exit\n";
+            std::cout << "Selection: ";
+            std::cin >> choice;
         }
     } while (choice != 6);
 
     return 0;
 }
 
-void loadData(string path, int bramImg[])
+bram loadData(string path)
 {
-
-    ifstream inFile;
+    bram loadedData;
+    std::ifstream inFile;
     int i = 0;
     int x;
     inFile.open(path);
     while (inFile >> x)
     {
-        &bramImg[i] = x;
-        i++;
+        loadData.push_back(x);
     }
     inFile.close();
+    return loadData;
 }
 
-void writeBramImg(const int bramArray[])
+void writeBramImg(bram bramImgArray)
 {
     FILE *bramImg;
     bramImg = fopen("/dev/bram_img", "w");
     for (int i = 0; i < BRAM_SIZE; ++i)
     {
-        fprintf(bramImg, "(%d,%d)\n", i, bramArray[i]);
+        fprintf(bramImg, "(%d,%d)\n", i, bramImgArray[i]);
     }
     fclose(bramImg);
 }
